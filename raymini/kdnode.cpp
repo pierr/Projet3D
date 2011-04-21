3e4a02c2177e3abe035af2af738664e236219874
Jan <delojoba@telecom-paristech.fr>
2011-04-22 00:59:10 +0200

    kdtree a nouveau. todo: mediane

--- raymini/kdnode.cpp
+++ raymini/kdnode.cpp
@@ -8,6 +8,7 @@ using namespace std;
 std::vector<kdleaf> kdnode::sort()
 {
     std::vector<kdleaf> supleafs;
+    std::vector<kdleaf> midleafs;
     std::vector<kdleaf> infleafs;
 
     //si le vecteur a plus d'un leaf on doit ordonner
@@ -15,7 +16,7 @@ std::vector<kdleaf> kdnode::sort()
     if(leafs.size()>1){
 
         //calcul rapide(?) de la mediane
-        float median;
+        float median = 0;
         float min_median = 0; //min_vecteur
         float max_median = FLT_MAX;
         for(unsigned int i=0; i<leafs.size(); i++){
@@ -28,22 +29,30 @@ std::vector<kdleaf> kdnode::sort()
                     if(jleaf.get_distance() < ileaf.get_distance()) count_under++;
                     else if(jleaf.get_distance() > ileaf.get_distance()) count_over++;
                 }
-                median = ileaf.get_distance();
+                cout << "under " << count_under << "\tmin_median " << min_median << "\tmax_median " << max_median << "\tover " << count_over << endl;
                 if(count_under > count_over) max_median = ileaf.get_distance();
                 else if(count_under < count_over) min_median = ileaf.get_distance();
                 else break;
             }
         }
+        cout << "\tsize = " << leafs.size() << endl;
+        cout << "\tmedian = " << median << endl;
+        if(median==0) median=(min_median+max_median)/2;
+        cout << "\tmedian = " << median << endl;
+        sleep(2);
 
         // on divise notre vecteur en 2 vecteurs en coupant par la mediane
         for(unsigned int i=0; i<leafs.size(); i++){
             kdleaf ileaf = leafs.at(i);
+            cout << "\tileaf[" << i << "].distance = " << ileaf.get_distance() << endl;
             if(ileaf.get_distance() > median)
                 supleafs.push_back(leafs.at(i));
-            else
+            else if(ileaf.get_distance() < median)
                 infleafs.push_back(leafs.at(i));
+            else
+                midleafs.push_back(leafs.at(i));
         }
-        cout << "sup = " << supleafs.size() << "\tinf = " << infleafs.size() << endl;
+        cout << "sup = " << supleafs.size() << "\tmid = " << midleafs.size() << "\tinf = " << infleafs.size() << endl;
         leafs.clear();
 
         //on cree les deux nouveaux noeuds
@@ -51,18 +60,20 @@ std::vector<kdleaf> kdnode::sort()
         kdnode infnode = kdnode(infleafs);
 
         //les deux noeuds ordonnent chaque vecteur
+        cout << "supnode" << endl;
         supleafs = supnode.sort();
+        cout << "infnode" << endl;
         infleafs = infnode.sort();
         delete &supnode;
         delete &infnode;
 
-        //on rassemble les deux vecteurs
-        for(unsigned int i=0; i<infleafs.size(); i++){
+        //on rassemble les vecteurs
+        for(unsigned int i=0; i<infleafs.size(); i++)
             leafs.push_back(infleafs.at(i));
-        }
-        for(unsigned int i=0; i<supleafs.size(); i++){
+        for(unsigned int i=0; i<midleafs.size(); i++)
+            leafs.push_back(midleafs.at(i));
+        for(unsigned int i=0; i<supleafs.size(); i++)
             leafs.push_back(supleafs.at(i));
-        }
     }
     return leafs;
 }
